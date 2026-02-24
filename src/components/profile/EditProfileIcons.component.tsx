498fc7713cdcf8dbad0d53f6c69c9f8a4925e9de
Bao Nguyen <baogavip00@gmail.com>
2026-02-24 13:02:35 +0700

    fix: update user profile page

---
+++ src/components/profile/EditProfileIcons.component.tsx
@@ -0,0 +1,18 @@
+export function EditAvatarIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-6 h-6 text-white opacity-0 group-hover:opacity-100 transition duration-300"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.232 5.232l3.536 3.536M9 13l6.768-6.768a2.5 2.5 0 113.536 3.536L12.536 16.536a4 4 0 01-1.414.943L8 18l.521-3.122A4 4 0 019 13z"
+      />
+    </svg>
+  );
+}
